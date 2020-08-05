558366fa426f4bf4ef236217fdad63d2d1e63533
stashymane <aestheticrice@gmail.com>
2020-08-05 19:50:04 +0300

    Remove legacy code

--- src/main/kotlin/dev/stashy/midibind/Main.kt
+++ src/main/kotlin/dev/stashy/midibind/Main.kt
@@ -1,25 +1,4 @@
 package dev.stashy.midibind
 
-import dev.stashy.midibind.midi.Device
-import javax.sound.midi.MidiSystem
-
 fun main() {
-    val infos = MidiSystem.getMidiDeviceInfo()
-    val inputs = mutableListOf<Device>()
-    infos.forEachIndexed { index, info ->
-        println("$index - ${info.name}")
-    }
-    println("Select a device...")
-    val index = readLine()!!.toInt()
-    val info = infos[index]
-    val dev = MidiSystem.getMidiDevice(info)
-    val receiver = Device(info)
-
-    inputs += receiver
-    println("${info.name} connected")
-    dev.transmitters.forEach {
-        it.receiver = receiver
-    }
-    dev.transmitter.receiver = receiver
-    dev.open()
 }
--- src/main/kotlin/dev/stashy/midibind/midi/definitions/SysEvents.kt
+++
@@ -1,5 +0,0 @@
-package dev.stashy.midibind.midi.definitions
-
-object SysEvents {
-    class Clock() {}
-}
