bc80d0055143fd91e916bf849bb0f5011b3ec316
stashymane <aestheticrice@gmail.com>
2020-08-05 22:58:30 +0300

    Refactor device

--- src/main/kotlin/dev/stashy/midibind/midi/Device.kt
+++ src/main/kotlin/dev/stashy/midibind/midi/Device.kt
@@ -3,17 +3,29 @@ package dev.stashy.midibind.midi
 import dev.stashy.midibind.midi.definitions.MidiEvent
 import javax.sound.midi.MidiDevice
 import javax.sound.midi.MidiMessage
+import javax.sound.midi.MidiSystem
 import javax.sound.midi.Receiver
 
-class Device(val info: MidiDevice.Info) : Receiver {
-    val actions = mutableListOf<() -> Unit>()
+class Device(private val dev: MidiDevice) : Receiver {
+    constructor(info: MidiDevice.Info) : this(MidiSystem.getMidiDevice(info))
+
+    val executors = mutableListOf<Executor>()
+
+    init {
+        dev.transmitters.forEach { //TODO automatically open/close channels based on actions
+            it.receiver = this
+        }
+        dev.transmitter.receiver = this
+        dev.open()
+    }
 
     override fun send(message: MidiMessage?, timeStamp: Long) {
         if (message == null) return
-        var data = MidiEvent.convert(message.message.map { it.toInt() }.toTypedArray())
+        val data = MidiEvent.convert(message.message.map { it.toInt() }.toTypedArray())
+        executors.forEach { it.sendMessage(data) }
     }
 
     override fun close() {
-
+        dev.close()
     }
 }
