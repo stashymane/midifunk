0bf00e7542ec773d1c2ff4f44d2fd305d1303b3d
stashymane <aestheticrice@gmail.com>
2020-08-07 22:03:14 +0300

    Implement disabling executor

--- src/main/kotlin/dev/stashy/midibind/midi/Executor.kt
+++ src/main/kotlin/dev/stashy/midibind/midi/Executor.kt
@@ -3,10 +3,12 @@ package dev.stashy.midibind.midi
 import dev.stashy.midibind.midi.definitions.MidiEvent
 
 class Executor {
+    val enabled = true
     val mods = mutableListOf<(MidiEvent) -> MidiEvent?>()
     val actions = mutableListOf<(MidiEvent) -> Unit>()
 
     fun sendMessage(msg: MidiEvent) {
+        if (!enabled) return
         var temp: MidiEvent? = msg
         mods.forEach { f ->
             temp?.let { temp = f(it) }
