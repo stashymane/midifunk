667fffc0dee93acd96447bcd4ecbc99f4e564769
stashymane <aestheticrice@gmail.com>
2020-08-08 17:15:57 +0300

    Add setting lsb/msb

--- src/main/kotlin/dev/stashy/midibind/midi/ByteOps.kt
+++ src/main/kotlin/dev/stashy/midibind/midi/ByteOps.kt
@@ -3,5 +3,13 @@ package dev.stashy.midibind.midi
 val Int.msb: Int
     get() = this and 0xF0 shr 4
 
+fun Int.withMsb(b: Int): Int {
+    return (b and 0xF shl 4) + (this and 0xF)
+}
+
 val Int.lsb: Int
     get() = this and 0xF
+
+fun Int.withLsb(b: Int): Int {
+    return (b and 0xF) + (this and 0xF0)
+}
