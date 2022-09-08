6aa46a033c3a922ab5cebab90006d69924f505e2
stashymane <aestheticrice@gmail.com>
2022-09-08 04:06:38 +0300

    Rework stateflow

--- src/main/kotlin/dev/stashy/midifunk/EventReceiver.kt
+++ src/main/kotlin/dev/stashy/midifunk/EventReceiver.kt
@@ -1,25 +1,29 @@
 package dev.stashy.midifunk
 
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.flow.*
 import javax.sound.midi.MidiDevice
 import javax.sound.midi.MidiMessage
 import javax.sound.midi.Receiver
 
-val MidiDevice.from: SharedFlow<MidiEvent?>
+val MidiDevice.input: Flow<MidiEvent>
     get() {
-        (this.transmitter.receiver as? EventReceiver)?.let { return it.flow }
-        return EventReceiver(this).flow
+        val receiver = (transmitter.receiver as? EventReceiver) ?: EventReceiver(this@input)
+        return receiver.flow
     }
 
-fun MidiDevice.to(e: MidiEvent) {
+fun MidiDevice.output(e: MidiEvent) {
     receiver.send(e.convert(), e.timestamp)
 }
 
 class EventReceiver(private val dev: MidiDevice, setReceiver: Boolean = true) :
     Receiver {
 
-    val flow = MutableStateFlow<MidiEvent?>(null)
+    private val state = MutableStateFlow<MidiEvent?>(null)
+    private val shared = state.asSharedFlow()
+    val flow
+        get() = shared.onEach { if (it == null) throw CancellationException("Device ${dev.deviceInfo.name} has been closed.") }
+            .filterNotNull()
 
     init {
         if (setReceiver)
@@ -27,11 +31,11 @@ class EventReceiver(private val dev: MidiDevice, setReceiver: Boolean = true) :
     }
 
     override fun close() {
-        flow.tryEmit(null)
+        state.tryEmit(null)
     }
 
     override fun send(message: MidiMessage, timeStamp: Long) {
-        MidiEvent.convert(message.message.mapTo(mutableListOf()) { it.toInt() }, timeStamp).let(flow::tryEmit)
+        MidiEvent.convert(message.message.mapTo(mutableListOf()) { it.toInt() }, timeStamp).let(state::tryEmit)
     }
 }
 
--- src/test/kotlin/dev/stashy/midifunk/EventTests.kt
+++ src/test/kotlin/dev/stashy/midifunk/EventTests.kt
@@ -5,13 +5,14 @@ import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import javax.sound.midi.MidiDevice
 import javax.sound.midi.ShortMessage
 
 class EventTests {
     val message = ShortMessage()
     val event = MidiEvent.convert(message.message.mapTo(mutableListOf()) { it.toInt() })
 
-    val dev = TestDevice()
+    val dev: MidiDevice = TestDevice()
 
     @Test
     fun conversionTest() {
@@ -20,9 +21,15 @@ class EventTests {
     }
 
     @Test
-    fun fromTest() {
+    fun replayTest() {
         dev.transmitter.receiver.send(message, event.timestamp)
-        val result = runBlocking { dev.from.first() }
-        assertEquals(event, result, event.data.joinToString(":") + " != " + result?.data?.joinToString(":"))
+        val result = runBlocking { dev.input.first() }
+        assertEquals(
+            event,
+            result,
+            "Flow returned invalid result: expected ${event.data.joinToString(":")}, got "
+                    + result.data.joinToString(":")
+        )
+    }
     }
 }
