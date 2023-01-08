b52a0ae560edea4505b3abece674084f93a1a5a8
stashymane <aestheticrice@gmail.com>
2023-01-09 01:21:00 +0200

    Update jvmTarget setting method

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     `java-library`
     `maven-publish`
@@ -77,13 +75,6 @@ signing {
     sign(publishing.publications["mavenJava"])
 }
 
-
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+kotlin {
+    jvmToolchain(8)
 }
