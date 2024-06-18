f18b2f1a249ff16b91962c117535dd602f01fd79
stashymane <aestheticrice@gmail.com>
2024-06-18 18:57:49 +0300

    asdf

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
     alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.publish) apply false
 }
 
 allprojects {
--- events/build.gradle.kts
+++ events/build.gradle.kts
@@ -18,33 +18,3 @@ kotlin {
         }
     }
 }
-
-val releaseRepo: String by project.properties
-val snapshotRepo: String by project.properties
-val repo = if (version.toString().endsWith("SNAPSHOT")) snapshotRepo else releaseRepo
-val sonatypeUsername = project.findProperty("sonatypeUsername")?.toString()
-val sonatypePassword = project.findProperty("sonatypePassword")?.toString()
-
-publishing {
-    repositories {
-        maven {
-            name = "MavenCentral"
-            url = uri(repo)
-
-            credentials {
-                username = sonatypeUsername
-                password = sonatypePassword
-            }
-        }
-    }
-}
-
-val signingKeyId: String? by project
-val signingKey: String? by project
-val signingPassword: String? by project
-
-signing {
-    isRequired = false
-    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
-    sign(publishing.publications)
-}
